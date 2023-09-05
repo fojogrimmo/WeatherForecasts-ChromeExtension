f2baa2f137299b9fd4f4513e6846502c03fbb58e
fojogrimmo <afanasyvekarindm@gmail.com>
2023-09-05 10:56:30 +0300

    Add weather codes

--- js/content.js
+++ js/content.js
@@ -1,6 +1,7 @@
 // http://api.weatherapi.com/v1/current.json?key=163dbad5aa3b4fbfb49164654231608&q=London
 // https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=3
 const apiKey = "163dbad5aa3b4fbfb49164654231608";
+const defaultCity = "Moscow";
 
 const form = document.querySelector(".info-form");
 const inputField = document.querySelector(".info-input");
@@ -36,6 +37,7 @@ const tomorrowDayName = document.querySelector("#tomorrow-dayname");
 const afterTomorrowDayName = document.querySelector("#after-tomorrow-dayname");
 
 let city;
+
 const weekdays = [
   "Sunday",
   "Monday",
@@ -46,29 +48,50 @@ const weekdays = [
   "Saturday",
 ];
 
-const rainyCodes = [1063, 1183, 1186, 1189, 1192, 1195, 1198, 1201, 1246];
-const sunnyCodes = [1000];
-const partlyCloudyCode = [1003];
+const weatherCodes = {
+  rainy: [
+    1063, 1183, 1186, 1189, 1192, 1195, 1198, 1201, 1246, 1180, 1150, 1153,
+    1240, 1243,
+  ],
+  sunny: [1000],
+  cloudy: [1006, 1009, 1030, 1135],
+  partlyCloudy: [1003],
+  snowy: [
+    1066, 1069, 1072, 1114, 1147, 1168, 1171, 1204, 1207, 1210, 1213, 1216,
+    1219, 1222, 1225, 1237, 1249, 1252, 1255, 1258, 1261, 1264, 1117,
+  ],
+  stormy: [1282, 1279, 1276, 1273],
+};
 
 function clearInputField() {
   inputField.addEventListener("focus", () => {
-    inputField.value = " ";
+    inputField.value = "";
   });
 }
-clearInputField();
+
+function setWeatherIcon(iconElement, code) {
+  if (weatherCodes.rainy.includes(code)) {
+    iconElement.src = "/images/rainy.png";
+  } else if (weatherCodes.sunny.includes(code)) {
+    iconElement.src = "/images/sunny.png";
+  } else if (weatherCodes.cloudy.includes(code)) {
+    iconElement.src = "/images/cloudy.png";
+  } else if (weatherCodes.partlyCloudy.includes(code)) {
+    iconElement.src = "/images/partly-cloudy.png";
+  } else if (weatherCodes.snowy.includes(code)) {
+    iconElement.src = "/images/snowy.png";
+  } else if (weatherCodes.stormy.includes(code)) {
+    iconElement.src = "/images/stormy.png";
+  }
+}
 
 function getDefaultWeather() {
-  const defaultCity = "Moscow";
   const defaultUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${defaultCity}&days=3`;
 
   fetch(defaultUrl)
-    .then((response) => {
-      return response.json();
-    })
+    .then((response) => response.json())
     .then((data) => {
-      // TIME ---
       currentTime.textContent = data.location.localtime.slice(-5);
-
       // WEEKDAY ---
       const date = new Date(data.location.localtime);
       const weekdayIndex = date.getDay();
@@ -87,61 +110,44 @@ function getDefaultWeather() {
       const rearrangedDate = `${parts[1]} ${parts[0]} ${parts[2]}`;
       todayDate.textContent = rearrangedDate.replace(",", "");
 
-      // CITY NAME ---
       cityName.textContent = data.location.name;
-      // COUNTRY ---
       countryName.textContent = data.location.country;
-      // TEMP ---
       currentTemperature.textContent = data.current.temp_c.toFixed();
-      // WEATHER ST ---
       weatherStatus.textContent = data.current.condition.text;
-
-      // FL
       feelsLike.textContent = data.current.feelslike_c.toFixed();
-
       humidity.textContent = data.current.humidity;
       windSpeed.textContent = data.current.wind_kph;
 
       todayMaxTemp.textContent =
         data.forecast.forecastday[0].day.maxtemp_c.toFixed();
-
       todayMinTemp.textContent =
         data.forecast.forecastday[0].day.mintemp_c.toFixed();
-
       tomorrowMaxTemp.textContent =
         data.forecast.forecastday[1].day.maxtemp_c.toFixed();
       tomorrowMinTemp.textContent =
         data.forecast.forecastday[1].day.mintemp_c.toFixed();
-
       afterTomorrowMaxTemp.textContent =
         data.forecast.forecastday[2].day.maxtemp_c.toFixed();
       afterTomorrowMinTemp.textContent =
         data.forecast.forecastday[2].day.mintemp_c.toFixed();
 
-      const code = data.current.condition.code;
-      const forecastTodayCode = data.forecast.forecastday[0].day.condition.code;
-      const forecastTomorrowCode =
-        data.forecast.forecastday[1].day.condition.code;
-      const forecastAfterTomorrowCode =
-        data.forecast.forecastday[2].day.condition.code;
-
-      function setWeatherIcon(iconElement, code) {
-        if (rainyCodes.includes(code)) {
-          iconElement.src = "/images/rainy.png";
-        } else if (sunnyCodes.includes(code)) {
-          iconElement.src = "/images/sunny.png";
-        } else if (partlyCloudyCode.includes(code)) {
-          iconElement.src = "/images/partly-cloudy.png";
-        }
-      }
-
-      setWeatherIcon(weatherIcon, code);
-      setWeatherIcon(todayForecastIcon, forecastTodayCode);
-      setWeatherIcon(tomorrowForecastIcon, forecastTomorrowCode);
-      setWeatherIcon(afterTomorrowForecastIcon, forecastAfterTomorrowCode);
+      setWeatherIcon(weatherIcon, data.current.condition.code);
+      setWeatherIcon(
+        todayForecastIcon,
+        data.forecast.forecastday[0].day.condition.code
+      );
+      setWeatherIcon(
+        tomorrowForecastIcon,
+        data.forecast.forecastday[1].day.condition.code
+      );
+      setWeatherIcon(
+        afterTomorrowForecastIcon,
+        data.forecast.forecastday[2].day.condition.code
+      );
     });
 }
 
+clearInputField();
 getDefaultWeather();
 
 form.onsubmit = function (e) {
@@ -191,8 +197,6 @@ form.onsubmit = function (e) {
       humidity.textContent = data.current.humidity;
       windSpeed.textContent = data.current.wind_kph;
 
-      //
-
       todayMaxTemp.textContent =
         data.forecast.forecastday[0].day.maxtemp_c.toFixed();
 
@@ -209,26 +213,19 @@ form.onsubmit = function (e) {
       afterTomorrowMinTemp.textContent =
         data.forecast.forecastday[2].day.mintemp_c.toFixed();
       //
-      const code = data.current.condition.code;
-      const forecastTodayCode = data.forecast.forecastday[0].day.condition.code;
-      const forecastTomorrowCode =
-        data.forecast.forecastday[1].day.condition.code;
-      const forecastAfterTomorrowCode =
-        data.forecast.forecastday[2].day.condition.code;
 
-      function setWeatherIcon(iconElement, code) {
-        if (rainyCodes.includes(code)) {
-          iconElement.src = "/images/rainy.png";
-        } else if (sunnyCodes.includes(code)) {
-          iconElement.src = "/images/sunny.png";
-        } else if (partlyCloudyCode.includes(code)) {
-          iconElement.src = "/images/partly-cloudy.png";
-        }
-      }
-
-      setWeatherIcon(weatherIcon, code);
-      setWeatherIcon(todayForecastIcon, forecastTodayCode);
-      setWeatherIcon(tomorrowForecastIcon, forecastTomorrowCode);
-      setWeatherIcon(afterTomorrowForecastIcon, forecastAfterTomorrowCode);
+      setWeatherIcon(weatherIcon, data.current.condition.code);
+      setWeatherIcon(
+        todayForecastIcon,
+        data.forecast.forecastday[0].day.condition.code
+      );
+      setWeatherIcon(
+        tomorrowForecastIcon,
+        data.forecast.forecastday[1].day.condition.code
+      );
+      setWeatherIcon(
+        afterTomorrowForecastIcon,
+        data.forecast.forecastday[2].day.condition.code
+      );
     });
 };
